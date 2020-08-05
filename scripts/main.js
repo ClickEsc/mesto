c8f13ee7a6e23044c01eca9ff61d1affbc8e86ec
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2020-08-05 15:23:38 +0300

    Publish at gh-pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,39 @@
 let popup = document.querySelector('.popup')
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__close');
+let popupSaveButton = popup.querySelector('.popup__save');
 
-let popupToggle = function() {
+let popupToggle = function(event) {
   popup.classList.toggle('popup_opened');
 }
 
-/*popupToggle();*/
+let popupClose = function(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  } else {
+    popupToggle(event);
+  }
+}
+
+let formElement = document.querySelector('.popup__container');
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  let username = document.querySelector('.profile__username');
+  let bio = document.querySelector('.profile__bio');
+
+  let nameInput = popup.querySelector('.popup__username');
+  let bioInput = popup.querySelector('.popup__bio');
+
+  username.textContent = nameInput.value;
+  bio.textContent = bioInput.value;
+  
+  popupToggle(event);
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
 
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
-
-
-/*console.log({
-  popup,
-  popupOpenButton,
-  popupCloseButton,
-})*/
+popup.addEventListener('click', popupClose);
